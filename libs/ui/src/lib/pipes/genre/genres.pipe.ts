e29d0a856a4c374861ed37392660fe01e7e7a3a6
Petr Savelev <p.savelyev@kickidler.com>
2025-03-27 15:18:43 +0300

    Fixes for routerLInk. Links for author and books in title. New genre pipe and const implementation. - // fix for pipe injection

--- libs/ui/src/lib/pipes/genre/genres.pipe.ts
+++ libs/ui/src/lib/pipes/genre/genres.pipe.ts
@@ -1,11 +1,11 @@
-import { inject, Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { GenrePipe } from './genre.pipe';
 
 @Pipe({
   name: 'genres',
 })
 export class GenresPipe implements PipeTransform {
-  private genrePipe = inject(GenrePipe);
+  private genrePipe = new GenrePipe(); // injection is not working here
 
   transform(genres: string[]): string {
     return genres.map((genre) => this.genrePipe.transform(genre)).join(', ');
