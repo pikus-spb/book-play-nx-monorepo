d55fffffc5e210e27706039a7081e66757b571ed
Petr Savelev <p.savelyev@kickidler.com>
2025-05-05 03:45:52 +0300

    Added yandex tts voices support

--- libs/models/src/lib/tts-params.ts
+++ libs/models/src/lib/tts-params.ts
@@ -1,6 +1,13 @@
+export enum Voices {
+  Svetlana = 'ru-RU-SvetlanaNeural',
+  Dmitry = 'ru-RU-DmitryNeural',
+  Ermil = 'ermil',
+  Jane = 'jane',
+}
+
 export interface TtsParams {
   text: string;
   pitch: string | null;
   rate: string | null;
-  voice: string;
+  voice: Voices;
 }
