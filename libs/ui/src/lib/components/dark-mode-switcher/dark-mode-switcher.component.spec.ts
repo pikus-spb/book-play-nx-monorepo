90bc1d5dee74d0c4fc4ce62f0a9fefec38870d5a
Petr Savelev <p.savelyev@kickidler.com>
2025-05-12 21:25:23 +0300

    Added dark mode component tests

--- libs/ui/src/lib/components/dark-mode-switcher/dark-mode-switcher.component.spec.ts
+++ libs/ui/src/lib/components/dark-mode-switcher/dark-mode-switcher.component.spec.ts
@@ -1,16 +1,27 @@
+import { Renderer2 } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { DarkModeSwitcherComponent } from './dark-mode-switcher.component';
 
-xdescribe('DarkModeSwitcherComponent', () => {
+jest.mock('@book-play/utils-browser', () => ({
+  ...jest.requireActual('@book-play/utils-browser'),
+  isDarkMode: jest.fn(),
+  listenDarkModeChange: jest.fn(),
+}));
+
+describe('DarkModeSwitcherComponent', () => {
   let component: DarkModeSwitcherComponent;
   let fixture: ComponentFixture<DarkModeSwitcherComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DarkModeSwitcherComponent],
+      imports: [DarkModeSwitcherComponent, MatSlideToggleModule, MatIconModule],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(DarkModeSwitcherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +29,25 @@ xdescribe('DarkModeSwitcherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle dark mode when the toggle button is clicked', () => {
+    const mockRenderer = {
+      addClass: jest.fn(),
+      removeClass: jest.fn(),
+    };
+
+    // @ts-expect-error: private renderer: Renderer2;
+    component.renderer = mockRenderer as Renderer2;
+    // @ts-expect-error: protected isDarkMode: Signal<boolean>;
+    component.isDarkMode.set(false);
+    // @ts-expect-error: protected toggleDarkMode(): void;
+    component.toggleDarkMode();
+
+    expect(mockRenderer.addClass).toHaveBeenCalledWith(document.body, 'dark');
+    expect(mockRenderer.removeClass).toHaveBeenCalledWith(
+      document.body,
+      'light'
+    );
   });
 });
