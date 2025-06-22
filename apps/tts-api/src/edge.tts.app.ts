5d57befd467cfbb20b63da994f1900c1decae320
Petr Savelev <p.savelyev@kickidler.com>
2025-06-22 23:56:37 +0300

    Voices audio quality improvements for tts-api

--- apps/tts-api/src/edge.tts.app.ts
+++ apps/tts-api/src/edge.tts.app.ts
@@ -1,5 +1,11 @@
 import { TtsParams } from '@book-play/models';
-import { getRandomFileNames, pitch, rate } from '@book-play/utils-node';
+import {
+  equalize,
+  getRandomFileNames,
+  pitch,
+  rate,
+  removeSilence,
+} from '@book-play/utils-node';
 import { Blob } from 'buffer';
 import { spawn } from 'child_process';
 import fs from 'fs';
@@ -9,35 +15,9 @@ export default class EdgeTtsApp {
     return text.replace(/([^.]+)\.$/, '$1');
   }
 
-  private removeSilence(
-    fileName: string,
-    fileNameOut: string
-  ): Promise<string> {
-    const args = [
-      fileName,
-      '-C',
-      '48',
-      fileNameOut,
-      'silence',
-      '-l',
-      '1',
-      '0.001',
-      '1%',
-      '-1',
-      '0.6',
-      '1%',
-    ];
-    const removeSilenceProc = spawn('sox', args, { detached: true });
-    return new Promise((resolve) => {
-      removeSilenceProc.on('close', () => {
-        resolve(fileNameOut);
-      });
-    });
-  }
-
   public runTts(params: TtsParams): Promise<Blob> {
     const { text, voice } = params;
-    const files = getRandomFileNames(4, '.mp3');
+    const files = getRandomFileNames(5, '.mp3');
 
     const args = [];
     args.push(`--text="${this.normalizeText(text)}"`);
@@ -48,11 +28,16 @@ export default class EdgeTtsApp {
 
     return new Promise((resolve) => {
       ttsProc.on('close', async () => {
-        await this.removeSilence(files[0], files[1]);
+        await removeSilence(files[0], files[1]);
         await pitch(params.pitch, '24000', files[1], files[2]);
         await rate(params.rate, files[2], files[3]);
+        await equalize(
+          ['equalizer=f=7500:width_type=h:width=3500:g=-10'],
+          files[3],
+          files[4]
+        );
 
-        const buffer = fs.readFileSync(files[3]);
+        const buffer = fs.readFileSync(files[4]);
         const blob = new Blob([buffer]);
 
         resolve(blob);
