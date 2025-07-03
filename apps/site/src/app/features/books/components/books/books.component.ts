5341ca93f7ff7eb2c39612c2ca4595d2f6ba5fb3
Petr Savelev <p.savelyev@kickidler.com>
2025-07-03 18:57:23 +0300

    Implemented search query for book search component

--- apps/site/src/app/features/books/components/books/books.component.ts
+++ apps/site/src/app/features/books/components/books/books.component.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   inject,
-  signal,
-  WritableSignal,
+  model,
+  OnInit,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 import { MatFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ScrollbarDirective, TagLinkComponent } from '@book-play/ui';
 import { Store } from '@ngrx/store';
 import { LoadingThenShowDirective } from '../../../../shared/directives/loading-then-show/loading-then-show.directive';
@@ -26,28 +30,44 @@ import {
     ScrollbarDirective,
     TagLinkComponent,
     MatFabButton,
+    FormsModule,
   ],
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
   private store = inject(Store);
   protected data = this.store.selectSignal(bookSearchSelector);
   protected errors = this.store.selectSignal(bookSearchErrorsSelector);
-  protected query: WritableSignal<any> = signal<string>('');
+  protected query = model<string | null>('');
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
-  protected search(event: SubmitEvent): void {
-    this.store.dispatch(
-      bookSearchAction({
-        query: this.query(),
-      })
-    );
+  public ngOnInit(): void {
+    this.router.events
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.initSearch();
+        }
+      });
 
-    event.preventDefault();
+    this.initSearch();
   }
 
-  protected updateQuery(event: Event): void {
-    this.query.set((event.target as HTMLInputElement).value);
+  protected initSearch(): void {
+    this.query.set(this.route.snapshot.paramMap.get('search'));
+    this.store.dispatch(
+      bookSearchAction({
+        query: this.query() ?? '',
+      })
+    );
+  }
+
+  protected submit(event: Event): void {
+    this.router.navigateByUrl(`/books/${this.query()}`);
+    event.preventDefault();
   }
 }
