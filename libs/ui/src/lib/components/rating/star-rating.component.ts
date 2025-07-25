2351f3a64e6d761b0c8616cacbba540fb281ea34
Petr Savelev <p.savelyev@kickidler.com>
2025-07-25 14:39:10 +0300

    Added advanced search query string; names and paths refactoring;

--- libs/ui/src/lib/components/rating/star-rating.component.ts
+++ libs/ui/src/lib/components/rating/star-rating.component.ts
@@ -1,15 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { StarRatingModule } from 'angular-star-rating';
 
 @Component({
   selector: 'lib-star-rating',
-  imports: [StarRatingModule],
+  imports: [StarRatingModule, ReactiveFormsModule],
   templateUrl: './star-rating.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StarRatingComponent),
+      multi: true,
+    },
+  ],
 })
-export class StarRatingComponent {
+export class StarRatingComponent implements ControlValueAccessor {
   public rating = input.required<number>();
   public showLabel = input<boolean>(true);
-  protected readonly Number = Number;
-  protected readonly String = String;
-  protected readonly top = top;
+  public formControlName = input<string>('');
 }
