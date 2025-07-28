32c9b103c0401fbed695ae86d3d5c60a82bc1b7a
Petr Savelev <p.savelyev@kickidler.com>
2025-07-28 19:55:50 +0300

    Fixes for a correct search for genres

--- libs/ui/src/lib/components/author-genres-list/author-genres-list.component.ts
+++ libs/ui/src/lib/components/author-genres-list/author-genres-list.component.ts
@@ -1,16 +1,17 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatChip, MatChipSet } from '@angular/material/chips';
+import { RouterLink } from '@angular/router';
 import { Genre } from '@book-play/models';
 import { GenrePipe } from '../../pipes/genre/genre.pipe';
 
 @Component({
   selector: 'lib-author-genres-list',
-  imports: [GenrePipe, MatChipSet, MatChip],
+  imports: [GenrePipe, MatChipSet, MatChip, RouterLink],
   templateUrl: './author-genres-list.component.html',
   styleUrl: './author-genres-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthorGenresListComponent {
   public genres = input<Genre[]>();
-  public linkPrefix = input<string>('/author/genre');
+  public linkPrefix = input<string>('/advanced-search/genres=');
 }
