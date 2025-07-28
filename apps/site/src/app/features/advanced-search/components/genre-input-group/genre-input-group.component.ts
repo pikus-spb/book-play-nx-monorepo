9b85dc28cf4c01cd69b5d70b13dcc6ecb84e888c
Petr Savelev <p.savelyev@kickidler.com>
2025-07-28 03:16:37 +0300

    Auto open / close genre categories based on selection

--- apps/site/src/app/features/advanced-search/components/genre-input-group/genre-input-group.component.ts
+++ apps/site/src/app/features/advanced-search/components/genre-input-group/genre-input-group.component.ts
@@ -1,34 +1,45 @@
+import { AsyncPipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   input,
   OnInit,
 } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { FB2_GENRES } from '@book-play/constants';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FB2_GENRES, Fb2GenresGroup, isGenreGroup } from '@book-play/constants';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'genre-input-group',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './genre-input-group.component.html',
   styleUrl: './genre-input-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GenreInputGroupComponent implements OnInit {
-  public formGroup = input<FormGroup>();
-  public controlNames = input<string[]>();
+  public form = input<FormGroup>();
+  public controlNames = input<string[] | Fb2GenresGroup>();
   public subgroup = input<boolean>(false);
   public name = input.required<string>();
+  protected open?: Observable<boolean>;
 
-  public ngOnInit() {
-    this.controlNames()?.forEach((name) => {
-      this.formGroup()?.addControl(name, new FormControl(''));
-    });
+  ngOnInit() {
+    const group = this.controlNames();
+    if (group !== undefined) {
+      this.open = this.form()?.valueChanges.pipe(
+        map((value) => {
+          if (isGenreGroup(group)) {
+            return Object.keys(group).some((key) => {
+              return group[key].some((subkey) => {
+                return value[subkey];
+              });
+            });
+          } else {
+            return group.some((key) => value[key]);
+          }
+        })
+      );
+    }
   }
 
   protected readonly FB2_GENRES = FB2_GENRES;
