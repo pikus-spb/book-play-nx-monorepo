7d9a99e914d79be8229672f5a29666ac173ace01
Petr Savelev <p.savelyev@kickidler.com>
2025-08-12 15:49:54 +0300

    BOOK-8: Выводить подсказку "о книге" при наведении курсора - // fix test

--- apps/site/setup.jest.js
+++ apps/site/setup.jest.js
@@ -3,3 +3,4 @@ import { ReadableStream, TextDecoder, TextEncoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.ReadableStream = ReadableStream;
+global.MessagePort = class MessagePort {};
