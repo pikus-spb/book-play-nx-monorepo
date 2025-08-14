bdfd3ce84c6b545fc051803e2f3a2657452022f8
Petr Savelev <p.savelyev@kickidler.com>
2025-08-14 18:19:07 +0300

    BOOK-9: Доделать crawler с учетом новых изменений

--- apps/crawler/src/app.ts
+++ apps/crawler/src/app.ts
@@ -1,5 +1,6 @@
 import { UIAuthorToDBAuthor, UIBookToDBBook } from '@book-play/models';
-import { Fb2Parser } from '@book-play/utils-common';
+import { SearchBook } from '@book-play/scraper';
+import { error, Fb2Parser, log } from '@book-play/utils-common';
 import {
   getJsonGzFileName,
   saveContentsToZipFile,
@@ -8,6 +9,7 @@ import {
 import { environment } from 'environments/environment.ts';
 import mysql, { PoolOptions } from 'mysql2';
 import { addAdditionalDataToAuthor } from './author-info.ts';
+import { addAdditionalDataToBook } from './book-info.ts';
 import { saveToDataBase } from './db';
 import { queryAuthorId } from './db.ts';
 import { deleteFiles, findFiles } from './files.ts';
@@ -15,75 +17,90 @@ import { getBookData } from './parse.ts';
 import { checkBookDataIsValid } from './validation.ts';
 
 export const workingDirectory = __dirname + '/' + process.argv[2];
+const pool = mysql.createPool(environment.DB_CONFIG as unknown as PoolOptions);
 export const parser = new Fb2Parser();
-export const pool = mysql.createPool(
-  environment.DB_CONFIG as unknown as PoolOptions
-);
+const scrapper = new SearchBook();
+scrapper.init().then(() => run());
 
 export async function run(): Promise<void> {
+  log('\n\n\n');
+  log('Starting...');
+
   const zipFiles = await findFiles('.zip');
   for (const zipFile of zipFiles) {
-    if (await unzipFile(zipFile, workingDirectory)) {
-      const fb2Files = await findFiles('.fb2');
-      await parseFb2Files(fb2Files);
-      deleteFiles(fb2Files);
-      deleteFiles([zipFile]);
+    try {
+      if (await unzipFile(zipFile, workingDirectory)) {
+        const fb2Files = await findFiles('.fb2');
+        await parseFb2Files(fb2Files);
+        deleteFiles(fb2Files);
+        deleteFiles([zipFile]);
+      }
+    } catch (err) {
+      error(err);
     }
   }
 
   pool.end();
-  console.log('All done!');
+  await scrapper.finalize();
+
+  log('All done!');
 }
 
 async function parseFb2Files(results: string[]) {
   for (const file of results) {
     try {
       const { lang, book } = await getBookData(file);
-      if (lang.toLowerCase() === 'ru') {
+      if (lang.toLowerCase().match('ru')) {
         if (checkBookDataIsValid(book)) {
+          log('\n');
+          log(JSON.stringify(book.full));
           const dbBook = UIBookToDBBook(book);
           const dbAuthor = UIAuthorToDBAuthor(book.author);
-          const authorId = await queryAuthorId(dbAuthor.full);
+          const authorId = await queryAuthorId(pool, dbAuthor.full);
 
           if (authorId !== -1) {
             dbBook.authorId = String(authorId);
+            log('Author already known: [' + authorId + ']');
           } else {
+            log('Adding info about author "' + dbAuthor.full + '"...');
             await addAdditionalDataToAuthor(dbAuthor);
           }
 
-          saveToDataBase(dbBook, dbAuthor)
-            .then((insertedId) => {
-              if (insertedId) {
-                console.log('Added to database: ' + book.name);
+          log('Adding additional info about book...');
+          await addAdditionalDataToBook(scrapper, dbBook);
 
-                dbBook.id = insertedId;
-                const fileName = getJsonGzFileName(
-                  environment.BOOKS_JSON_PATH + insertedId
-                );
-                saveContentsToZipFile(
-                  JSON.stringify(dbBook.paragraphs),
-                  fileName
-                );
-                console.log('Compressed json to gzip file: ' + fileName + '\n');
-              }
-            })
-            .catch((err) =>
-              console.error((err.sqlMessage || err.message || err) + '\n')
-            );
+          try {
+            const insertedId = await saveToDataBase(pool, dbBook, dbAuthor);
+            if (insertedId) {
+              log('Added to database: ' + book.name);
+
+              dbBook.id = insertedId;
+              const fileName = getJsonGzFileName(
+                environment.BOOKS_JSON_PATH + insertedId
+              );
+              saveContentsToZipFile(
+                JSON.stringify(dbBook.paragraphs),
+                fileName
+              );
+              log('Compressed json to gzip file: ' + fileName + '\n');
+            }
+          } catch (err) {
+            error(err);
+          }
         } else {
-          console.log(
+          log(
             `Skipping "${book.name}" because book data is not valid "${book.full}". \n`
           );
         }
       } else {
-        console.log(
+        log(
           `Skipping "${
             book.name
           }" because in [ ${lang.toUpperCase()} ] language \n`
         );
       }
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      error(err);
     }
   }
 }
--- apps/crawler/src/parse.ts
+++ apps/crawler/src/parse.ts
@@ -1,10 +1,11 @@
 import { Book } from '@book-play/models';
+import { log } from '@book-play/utils-common';
 import { readFile } from '@book-play/utils-node';
 import { CheerioAPI } from 'cheerio';
 import { parser } from './app';
 
 export async function jquery(file: string): Promise<CheerioAPI> {
-  console.log('Parsing ' + file + '..');
+  log('Parsing ' + file + '..');
   const text = await readFile(file);
   return parser.loadCheeroApi(text);
 }
